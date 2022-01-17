88f690a2e3f7bbe8e657fdfcfe8347dbde4f6c0a
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-17 15:40:51 -0300

    update index.js

--- index.js
+++ index.js
@@ -22,11 +22,13 @@ firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
 client.on("messageCreate", async (message) => {
-require("./Handler.js")(client, message, database, config)
+require("./Handler.js")(client, message, database, config);
+
 });
 
 client.on("ready", () => {
 require("./Eventos/Ready.js")(client)
+require("./LoadCommands.js")(client)
 });
 
 client.login("OTIyNzE4ODk3NzgwNzExNDY1.YcFi8A.PGPPDCoVnPO1zw0KWDP9qRh_e9Y");
