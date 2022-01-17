9c7405bf4b39731e94477adc8aed74e1e30ea5df
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-17 14:41:57 -0300

    update Handler.js

--- Handler.js
+++ Handler.js
@@ -2,24 +2,65 @@ const Discord = require("discord.js")
 const config = require("./config.json")
 module.exports = async(client, message, database) => {
   
-if (message.author.bot) return;
-if (message.channel.type == 'dm') return;
+let dbPref = await database.ref(`Servidores/${message.guild.id}`).once('value');
+  let prefix = dbPref.val() ? dbPref.val().prefix ? dbPref.val().prefix : config.prefix.toLowerCase() : config.prefix.toLowerCase();
 
-let prefix = await client.db.get(`Servidores/${message.author.id}/prefix`)
-if (!prefix) prefix = config.prefix;
+	if(message.author.bot) return;
+  if(message.channel.type == "dm") return;
+  if(!message.content.toLowerCase().startsWith(config.prefix.toLowerCase())) return;
 
-if(!message.content.toLowerCase().startsWith(prefix.toLowerCase())) return;
+  let args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  let cmd = args.shift();
+  let comando = client.commands.get(cmd);
+  if(!comando) return;
+	
+  let embed1 = new Discord.MessageEmbed()
+.setAuthor(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
+.setDescription(`**Novo Comando Utilizado Por:** \`${message.author.tag} | ${message.author.id}\`\n\n**Comando Utilizado:** \`${config.prefix}${cmd}\`\n**Depois do Comando:** \`${args.length ? args.join(' ') : " "}\`\n\n**Servidor:** \`${message.guild.name} | ${message.guild.id}\`\n**Canal:** \`${message.channel.name} | ${message.channel.id}\`\n**ID da Mensagem:** \`${message.id}\`\n\n**Informações do servidor:**\n>  Usuários: \`${message.guild.members.cache.filter(u => !u.user.bot).size} Membros\` e \`${message.guild.members.cache.filter(u => u.user.bot).size} Bots\`\n>  Canais: \`${message.guild.channels.cache.size}\`\n>  Cargos: \`${message.guild.roles.cache.size}\``)
+    .setColor(`#0D02FA`)
+    .setTimestamp();
 
-if(message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) return;
+  let channel = client.channels.cache.get("916823908613771264")
 
-const args = message.content.slice(prefix.length).trim().split(/ +/g);
-const economia = args.shift().toLowerCase();
-const EcoFile = require(`..Economia/${economia}.js`)
+	/*if(!config.owners.includes(message.author.id)) 
+    
+message.reply(`**Nesse momento Eu estou sendo atualizado, Você não poderá usar meus comandos até a atualização acabar. Para mais informações entre em meu Servidor de Suporte usando \`.invite\`**`)
+return;
+}*/
+
+  let user1 = message.author;
+  let dbs = await   
+database.ref(`Start/${user1.id}`).once('value');
+  let dbsref = database.ref(`Start/${user1.id}`);
+
+  let db1 = await   
+database.ref(`Versao/${user1.id}`).once('value');
+  let db1ref = database.ref(`Versao/${user1.id}`);
+
+  let db2 = await   
+database.ref(`VersaoBuild`).once('value');
+  let d2ref = database.ref(`VersaoBuild`);
+
+  if (dbs.val() == null) {
+  message.reply(`**Antes de começar a usar minha Economia você deve usar \`${prefix}start\` Para liberar meus comandos de Economia**`)
+return;
+    }
 
   
+if (db1.val().versão !== db2.val().build) {
+message.reply(`**Tem uma nova Versão Disponível para sua Conta. Use \`${prefix}update\` Para aproveitar a nova Versão. Para mais informações entre em meu Servidor de Suporte \`${prefix}invite\`**`)
+return;
+}
+	
+   
+ channel.send({embeds: [embed1]})
+ reqEvent("xp")(client, message )
+ reqEvent("ItensRPG")(client, message)
+ reqEvent("QuestRPG")(client, message, args, database)
 try {
-    EcoFile.run(client, message, args, database, prefix);
+    comando.run(client, message, args, database, prefix);
   } catch(err) {
     console.log(err);
   }
+
 }
