b5f75010787d1bf61446aa3c96cac221bbbc988e
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-17 14:51:54 -0300

    update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ console.log("Comandos carregados!")
 
 
 client.on("messageCreate", async (message) => {
-require("./Handler.js")(client, database)
+require("./Handler.js")(client,message,database)
 });
 
 client.login("ODQ0MjI3ODk1ODE5ODk0Nzk0.YKPWfw.fFiPSooxWFVkZTQw7_yYgF6U0EE");
