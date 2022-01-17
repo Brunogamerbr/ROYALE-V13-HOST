d78ec8224be22736098eb45315cd8e55d66deff1
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-17 05:14:19 -0300

    update index.js

--- index.js
+++ index.js
@@ -10,11 +10,22 @@ client.aliases = new Discord.Collection();
 client.on("ready", () => {
 require("./Eventos/Ready.js")(client)
 require("./LoadCommands.js")(client)
-require("./firebase.js")(client, firebase)
 const DatabaseUtil = require("./DatabaseUtil.js");
 client.db = new DatabaseUtil({
   serverKey: "832judw.21ud.jiq1456f"
 });
+const firebaseConfig = {
+  apiKey: "AIzaSyAgv6EICjfRgQJPTvsEJPjTPwYl_Rq2d5U",
+  authDomain: "royale-bot-7b344.firebaseapp.com",
+  databaseURL: "https://royale-bot-7b344-default-rtdb.firebaseio.com",
+  projectId: "royale-bot-7b344",
+  storageBucket: "royale-bot-7b344.appspot.com",
+  messagingSenderId: "948007874893",
+  appId: "1:948007874893:web:ac0626c78346936721c704",
+  measurementId: "G-LT7353D3BV"
+};
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
 })
 
 client.on("messageCreate", async message => {
