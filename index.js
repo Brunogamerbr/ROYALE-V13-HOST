67f557dc09016bc9be2275a72025d701da4c3b27
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-17 16:33:00 -0300

    update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ require("./Handler.js")(client, message, database, config);
 
 client.on("ready", () => {
 require("./Eventos/Ready.js")(client)
-require("./LoadCommands.js")(client)
+require("./LoadAventura.js")(client)
 });
 
 client.login("OTIyNzE4ODk3NzgwNzExNDY1.YcFi8A.PGPPDCoVnPO1zw0KWDP9qRh_e9Y");
