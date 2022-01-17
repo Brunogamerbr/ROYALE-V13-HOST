60be67a0f1b37c80fe3e47405934e02a0e8dbe3f
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-17 05:17:36 -0300

    Create setvip.js

---
+++ Moderação/setvip.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js")
+
+exports.run = async (client, message, args, database) => {
+
+
+
+let dbPref = database.ref(`Servidores/${message.guild.id}`);
+	let dbP = await database.ref(`Servidores/${message.guild.id}`).once('value');
+	if (dbP.val() == null) {
+		dbref.set({ prefix: config.prefix})
+
+
+  }
+
+
+
+
+
+  
+
+
+let dbref = database.ref(`Vips/${message.guild.id}`);
+
+
+if (!message.member.hasPermission(['MANAGE_ROLES'])) { return message.inlineReply('<:erro:858615784771551252>| Você não pode usar esse comando!') }
+
+
+
+
+  
+let role = message.mentions.roles.first() || message.guild.roles.cache.get(args[0]);
+if(!role) return message.reply(`Cargo invalido!`);
+
+dbref.set({
+  cargoId: role.id
+}) 
+
+
+
+message.inlineReply(`**☑️| Cargo setado com sucesso! Use \`${dbP.val().prefix}addvip [@mencão]\` para adicionar o VIP Em algum membro!**`)
+
+
+
+}
