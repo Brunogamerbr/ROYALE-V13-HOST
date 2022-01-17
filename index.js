7223c349f1ac2d8b3e12d9b00ffc5cf4a3b3b983
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-17 05:12:12 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
+const firebase = require("firebase")
 const fs = require("fs");
 const client = new Discord.Client({intents: config.intents})
 client.tryes = new Discord.Collection();
@@ -9,10 +10,15 @@ client.aliases = new Discord.Collection();
 client.on("ready", () => {
 require("./Eventos/Ready.js")(client)
 require("./LoadCommands.js")(client)
+require("./firebase.js")(client, firebase)
+const DatabaseUtil = require("./DatabaseUtil.js");
+client.db = new DatabaseUtil({
+  serverKey: "832judw.21ud.jiq1456f"
+});
 })
 
 client.on("messageCreate", async message => {
-require("./Handler.js")(client,config)
+require("./Handler.js")(client,config,message)
 })
 
 client.login("OTIyNzE4ODk3NzgwNzExNDY1.YcFi8A.PGPPDCoVnPO1zw0KWDP9qRh_e9Y");
