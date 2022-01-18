176b8dd654da7e6be6ad564a1729d2e8fb7e04e1
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-18 07:23:40 -0300

    update Ready.js

--- Eventos/Ready.js
+++ Eventos/Ready.js
@@ -1,12 +1,13 @@
 const Discord = require("discord.js")
 let config = require("../config.json");
 module.exports = async (client, database) => {
+  
   global.ramUsoMax = 0;
   global.ramUsoMin = 999999;
   global.ramUso = parseFloat((process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2));
   
   setInterval(() => {
-    let usage = parseFloat((process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2))
+  let usage = parseFloat((process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2))
     
     if(usage > global.ramUsoMax) global.ramUsoMax = usage;
     if(usage < global.ramUsoMin) global.ramUsoMin = usage;
