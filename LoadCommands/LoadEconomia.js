4ae430622f40eba914de55f25d33ae8e71740671
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-18 01:21:54 -0300

    update LoadEconomia.js

--- LoadCommands/LoadEconomia.js
+++ LoadCommands/LoadEconomia.js
@@ -5,15 +5,15 @@ module.exports = async (client) => {
 let dir = __dirname
 client.commands = new Discord.Collection();
   
-const FireSimple = require("./DatabaseUtil.js");
+const FireSimple = require("../DatabaseUtil.js");
   client.db = new FireSimple({
     apiKey: "AIzaSyAgv6EICjfRgQJPTvsEJPjTPwYl_Rq2d5U",
     databaseURL: "https://royale-bot-7b344-default-rtdb.firebaseio.com"
   });
 
-  const cmds = fs.readdirSync(join(__dirname, "./Economia")).filter(file => file.endsWith(".js"));
+  const cmds = fs.readdirSync(join(__dirname, "../Economia")).filter(file => file.endsWith(".js"));
   for(let file of cmds) {
-    let cmd = require(join(__dirname, "./Economia", `${file}`));
+    let cmd = require(join(__dirname, "../Economia", `${file}`));
     client.commands.set(`${file}`.replace(".js", ""), cmd);
     if(cmd.conf && cmd.conf.aliases) {
       cmd.conf.aliases.forEach(alias => {
