f6cd4790c508cc8236f7fbd9e48be0076afa709f
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-18 08:24:26 -0300

    update addvip.js

--- Moderação/addvip.js
+++ Moderação/addvip.js
@@ -7,15 +7,15 @@ module.exports.run = async (client, message, args, database, prefix) => {
  
   if(!unperm.permissions.has(Permissions.FLAGS.MANAGE_ROLES))return message.reply("<:erro:858615784771551252>| Eu estou sem permissão de \`Gerenciar cargos\` nesse servidor!");
 
- let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
- if (!member) return message.reply('<:erro:858615784771551252>| Para poder executar o comando, tem que mencionar um membro!');
+  let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+  if (!member) return message.reply('<:erro:858615784771551252>| Para poder executar o comando, tem que mencionar um membro!');
 
-    let db = await database.ref(`Vips/${message.guild.id}`).once('value');
-    let role = db.val() != null ? message.guild.roles.cache.get(db.val().cargoId) : null;
+  let db = await database.ref(`Vips/${message.guild.id}`).once('value');
+  let role = db.val() != null ? message.guild.roles.cache.get(db.val().cargoId) : null;
 
-    if(!role) return message.reply(`Desculpe, mas o cargo de VIP não está definido ou está inválido! Para definir um cargo de VIP use \`${prefix}setvip\``);
+  if(!role) return message.reply(`Desculpe, mas o cargo de VIP não está definido ou está inválido! Para definir um cargo de VIP use \`${prefix}setvip\``);
 
-    let vipmessage = new Discord.MessageEmbed()
+  let vipmessage = new Discord.MessageEmbed()
       .setTitle("☑️ | Vip adicionado")
       .setDescription(`Vip Adicionado Por: \`${message.author.username}\`\n${member} recebeu o ${role}`)
       .setColor('#0D02FA')
