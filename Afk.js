7c3e7e46e9f2753614ce8340dbabd2d6cf28ea8c
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-18 03:44:10 -0300

    Create Afk.js

---
+++ Afk.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js")
+const db = require("quick.db")
+module.exports.run = async(client, message, database, prefix) => {
+  let afk = new db.table("AFKs"),
+  authorStatus = await afk.fetch(message.author.id),
+  mentioned = message.mentions.users.first();
+  if (mentioned && !authorStatus) {
+  let status = await afk.fetch(mentioned.id);
+  if (status) {
+  message.channel.send(`O Usúario **${mentioned.tag}** está AFK!\nMotivo: **\` ${status} \`**`)}}
+  if (authorStatus) {   
+  message.channel.send(`${message.author.tag} você não está está mais AFK.`);
+  afk.delete(message.author.id)
+}
+}
