7070bb8bb0f6f5ae2a87d182e2fe940cabdf938e
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-18 06:02:54 -0300

    update addvip.js

--- Moderação/addvip.js
+++ Moderação/addvip.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args, database, prefix) => {
   
  if(!message.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES))return message.reply("<:erro:858615784771551252>| Voce Não tem permissão para Usar esse comando!");
  
-  if(!client.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES))return message.reply("<:erro:858615784771551252>| Eu estou sem permissão de `\`Gerenciar cargos\` nesse servidor!");
+  if(!guild.me.permissions.has(Permissions.FLAGS.MANAGE_ROLES))return message.reply("<:erro:858615784771551252>| Eu estou sem permissão de `\`Gerenciar cargos\` nesse servidor!");
 
  let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
  if (!member) return message.reply('<:erro:858615784771551252>| Para poder executar o comando, tem que mencionar um membro!');
