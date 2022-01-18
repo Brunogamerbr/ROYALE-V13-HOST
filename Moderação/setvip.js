3b40f0d03eed02bdeefea7a552ea0d5a26bbb979
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-18 05:14:20 -0300

    update setvip.js

--- Moderação/setvip.js
+++ Moderação/setvip.js
@@ -7,7 +7,6 @@ let dbPref = database.ref(`Servidores/${message.guild.id}`);
 	if (dbP.val() == null) {
 		dbref.set({ prefix: config.prefix})
   }
-
 let dbref = database.ref(`Vips/${message.guild.id}`);
 if(!message.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES)) {
  return message.reply('<:erro:858615784771551252>| Você não pode usar esse comando!')}
@@ -15,9 +14,7 @@ if(!message.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES)) {
 let role = message.mentions.roles.first() || message.guild.roles.cache.get(args[0]);
 if(!role) return message.reply(`<:erro:858615784771551252>| Você precisa mencionar ou usar o id de um cargo invalido!`);
 
-dbref.set({
-  cargoId: role.id
-})
+dbref.set({cargoId: role.id})
 
-message.reply(`**☑️| Cargo setado com sucesso! Use \`${dbP.val().prefix}addvip [@mencão]\` para adicionar o VIP Em algum membro!**`)
+message.reply(`☑️| Cargo setado com sucesso! Use \`${dbP.val().prefix}addvip [@mencão]\` para adicionar o VIP Em algum membro!`)
 }
