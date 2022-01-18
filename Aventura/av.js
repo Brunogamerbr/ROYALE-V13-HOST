0fdff35847b871de69068e60085252cacf546596
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-18 07:52:57 -0300

    update av.js

--- Aventura/av.js
+++ Aventura/av.js
@@ -3,20 +3,16 @@ exports.run = async (client, message, args, database, prefix) => {
 
 let start = await client.db.get(`StartRPG_${message.author.id}`);
 if (start == null) {
-    return message.reply(`<:erro:858615784771551252>| Parece que você ainda não criou seu avatar, utilize \`${prefix}create-avatar\``)
-}	
+return message.reply(`<:erro:858615784771551252>| Parece que você ainda não criou seu avatar, utilize \`${prefix}create-avatar\``)}	
 	
-  let user = message.mentions.users.first() || client.users.cache.get(args[0]) || client.users.cache.find(u => u.tag === args.join(" ")) || message.author;
+let user = message.mentions.users.first() || client.users.cache.get(args[0]) || client.users.cache.find(u => u.tag === args.join(" ")) || message.author;
 
   let money = await client.db.get(`Economia_${user.id}`) || 0;
   let bank = await client.db.get(`Banco_${user.id}`) || 0;
-
   let level_sys = await client.db.get(`Nivel_${user.id}`) || { xp: 0, level: 1 };
-
   let xp = level_sys.xp, level = level_sys.level, nextLvl = level_sys.level * 340;
   
   let progress_bar = getProgress(xp, level, nextLvl);
-
 
 function getProgress(xp, level, nextLvl) {
   let percent = parseInt((xp / nextLvl) * 100);
@@ -24,10 +20,8 @@ function getProgress(xp, level, nextLvl) {
   let txt = ""
   if(percent >= 10) {
     txt += "<:11:913882717140181083>"
-
     txt += "<:12:913882733686689812>".repeat((p2 - 1) || 0);
     txt += "<:20:913882772857315348>".repeat(9 - p2);
-
     txt+= "<:30:913882784483901440>";
   } else {
     txt = "<:10:913882754524004352><:20:913882772857315348><:20:913882772857315348><:20:913882772857315348><:20:913882772857315348><:20:913882772857315348><:20:913882772857315348><:20:913882772857315348><:20:913882772857315348><:30:913882784483901440>"
@@ -37,7 +31,7 @@ function getProgress(xp, level, nextLvl) {
     
    let db = await client.db.get(`StartRPG_${user.id}`)
 
-if (db == null) {
+   if (db == null) {
     message.reply(`**<:erro:858615784771551252>| Esse usuário não Foi encontrado no meu mundo RPG!**`)
     return;
 }
