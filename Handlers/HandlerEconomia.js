6ef22ca783ddc24d9e8acec7473ab1149a5ac1b4
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-18 04:33:32 -0300

    update HandlerEconomia.js

--- Handlers/HandlerEconomia.js
+++ Handlers/HandlerEconomia.js
@@ -11,7 +11,7 @@ module.exports = async (client, message, database, config) => {
 
   let args = message.content.slice(prefix.length).trim().split(/ +/g);
   let cmd = args.shift();
-  let comando = client.economia.get(cmd);
+  let comando = client.economia.get(eco);
   if(!comando) return;
 	
   let embed1 = new Discord.MessageEmbed()
