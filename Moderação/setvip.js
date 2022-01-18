8f832c75956898f53d4dbac60229b971966f94ce
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-18 05:08:54 -0300

    update setvip.js

--- Moderação/setvip.js
+++ Moderação/setvip.js
@@ -13,7 +13,7 @@ if(!message.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES)) {
  return message.reply('<:erro:858615784771551252>| Você não pode usar esse comando!')}
 
 let role = message.mentions.roles.first() || message.guild.roles.cache.get(args[0]);
-if(!role) return message.reply(`Cargo invalido!`);
+if(!role) return message.reply(`<:erro:858615784771551252>| Você precisa mencionar ou usar o id de um cargo invalido!`);
 
 dbref.set({
   cargoId: role.id
