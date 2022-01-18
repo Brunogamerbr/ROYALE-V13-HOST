a3faec27eae58969002111ec09dc27330e7e2e50
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-18 08:07:57 -0300

    update Xp.js

--- Eventos/Xp.js
+++ Eventos/Xp.js
@@ -6,18 +6,8 @@ let dbN = await client.db.get(`Nivel/${message.author.id}`)
 if(dbN == null) {
 client.db.set(`Nivel/${message.author.id}`, {xp: 0}, {nivel: 1});
   }else {
-    
 client.db.set(`Nivel/${message.author.id}`, {xp: dbN.xp + Math.floor(Math.random() * (15 - 3)) + 3});
 if(dbN.level * 340 <= dbN.xp) {
 client.db.set(`Nivel/${message.author.id}`, {xp: 0}, {nivel: dbN.nivel + 1});
 }}
-
-const db = await client.db.get(`StartRPG_${message.author.id}`)
-    if(!db) return;
-    const hp = Number(db.hp)
-    if(hp <= 0) {
-    await client.db.delete(`StartRPG_${message.author.id}`)
-    await client.db.delete(`InventarioRPG_${message.author.id}`)
-   return message.reply(`${message.author} Você acabou perdendo todos os corações de **HP** você morreu... mas tente iníciar uma nova jornada!`).catch(e => console.log(e.message))
-    }
 }
