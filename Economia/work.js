e17756eb7a8a43dd7f6a7151018df53959e7a84e
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-18 08:34:36 -0300

    update work.js

--- Economia/work.js
+++ Economia/work.js
@@ -10,11 +10,11 @@ database.ref(`Start/${user1.id}`).once('value');
   let db1V = await   
 database.ref(`Versao/${user1.id}`).once('value');
   let db1Vref = database.ref(`Versao/${user1.id}`);
-
+  
   let db2B = await   
 database.ref(`VersaoBuild`).once('value');
   let d2Bref = database.ref(`VersaoBuild`);
-
+  
   if (dbs.val() == null) {
 message.reply(`**Antes de começar a usar minha Economia você deve usar \`${prefix}start\` Para liberar meus comandos de Economia**`)
 return;
@@ -35,10 +35,7 @@ return;
     let db2ref = database.ref(`Empregos/${user.id}`);
   
 
-  // Lista funções de um Programador irá fazer
    let programmer = ['BOT para Discord', 'Aplicativo para celular','Site profissional para seu Bot', 'Comando para seu Bot', 'Aplicativo para ver o Tempo', 'Site para sua Empresa'];
-   
-  // Lista funções de um miner
    let miner = ['Ouro', 'Diamante', 'Aluminio', 'Rubi', 'Safira', 'Esmeralda', 'Ametista', 'Topázio', 'Turqueza', 'Quartzo'];
 
    let timeout = (3.6e+6);
@@ -49,10 +46,8 @@ return;
     message.reply(`**<:erro:858615784771551252>| Você ja trabalhou recentemente. Você podera trabalhar novamente em: ${time.minutes}m ${time.seconds}s**`)
   } else {
 
-    if (db2.val().emprego == null || db2.val().emprego == 0) {
-    return message.reply(`**Para poder trabalhar você deve possuir um emprego \`${prefix}empregos\`.**`)
-}
-
+   if (db2.val().emprego == null || db2.val().emprego == 0) {
+    return message.reply(`**Para poder trabalhar você deve possuir um emprego \`${prefix}empregos\`.**`)}
     let tb = await database.ref(`Transacoes/${message.author.id}`).once("value")
     let tbref = database.ref(`Transacoes/${message.author.id}`)
     let t1 = tb.val() ? tb.val().t : [];
@@ -62,14 +57,14 @@ return;
     })
 
 
-    if (db2.val().emprego == 1) {
+   if (db2.val().emprego == 1) {
       let embed = new Discord.MessageEmbed()      
       .setAuthor(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
       .setDescription(`Você programou um: **${programmer[Math.floor(Math.random() * programmer.length)]}**. na venda você recebeu: **R$${quantia}** + **60** de **XP**`)     .setThumbnail(`https://i.imgur.com/IzCaEQc.png`)
       .setFooter(`Programação realizada`)
       .setColor(`#0D02FA`)
       .setTimestamp()
-       message.reply({embeds: [embed]})
+      message.reply({embeds: [embed]})
       db1ref.update({ dinheiro: db1.val().dinheiro + quantia })
       dbref.update({ work: Date.now() })  
   dbXref.update({ xp: dbX.val().xp + 60 })
@@ -81,10 +76,9 @@ return;
       .setDescription(`Você minerou um: **${miner[Math.floor(Math.random() * miner.length)]}**. você vendeu e faturou um lucro de **R$${quantia}** + **60** de **XP**`)
       .setThumbnail(`https://i.imgur.com/ux7cbLB.png`)
       .setFooter(`Mineraçao realizada`)
-.setColor(`#0D02FA`)
+      .setColor(`#0D02FA`)
       .setTimestamp()
       message.reply({embeds: [embed2]})
-      
       dbXref.update({ xp: dbX.val().xp + 60 })
       db1ref.update({ dinheiro: db1.val().dinheiro + quantia })
       dbref.update({ work: Date.now() })
