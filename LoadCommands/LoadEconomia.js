8972e375c32290d2de80a4d7ff3a01e319419708
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-18 02:18:21 -0300

    update LoadEconomia.js

--- LoadCommands/LoadEconomia.js
+++ LoadCommands/LoadEconomia.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 const { join } = require("path")
 module.exports = async (client) => {
 let dir = __dirname
-client.commands = new Discord.Collection();
+client.economia = new Discord.Collection();
   
 const FireSimple = require("../DatabaseUtil.js");
   client.db = new FireSimple({
@@ -13,11 +13,11 @@ const FireSimple = require("../DatabaseUtil.js");
 
   const cmds = fs.readdirSync(join(__dirname, "../Economia")).filter(file => file.endsWith(".js"));
   for(let file of cmds) {
-    let cmd = require(join(__dirname, "../Economia", `${file}`));
-    client.commands.set(`${file}`.replace(".js", ""), cmd);
+    let economia = require(join(__dirname, "../Economia", `${file}`));
+    client.economia.set(`${file}`.replace(".js", ""), economia);
     if(cmd.conf && cmd.conf.aliases) {
       cmd.conf.aliases.forEach(alias => {
-        client.commands.set(alias, cmd);
+        client.economia.set(alias, economia);
       })
     }
   }
