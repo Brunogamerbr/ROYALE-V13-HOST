0a1fa46dc0d54f415c888d836a5930d4eedebddd
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-18 05:16:11 -0300

    update setvip.js

--- Moderação/setvip.js
+++ Moderação/setvip.js
@@ -1,20 +1,20 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 const { Permissions } = require('discord.js');
-exports.run = async (client, message, args, database) => {
+module.exports.run = async (client, message, args, database, prefix) => {
   
 let dbPref = database.ref(`Servidores/${message.guild.id}`);
-	let dbP = await database.ref(`Servidores/${message.guild.id}`).once('value');
-	if (dbP.val() == null) {
-		dbref.set({ prefix: config.prefix})
-  }
+let dbP = await database.ref(`Servidores/${message.guild.id}`).once('value');
+if (dbP.val() == null) {
+dbref.set({ prefix: config.prefix})}
+
 let dbref = database.ref(`Vips/${message.guild.id}`);
 if(!message.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES)) {
- return message.reply('<:erro:858615784771551252>| Você não pode usar esse comando!')}
+return message.reply('<:erro:858615784771551252>| Você não pode usar esse comando!')}
 
 let role = message.mentions.roles.first() || message.guild.roles.cache.get(args[0]);
 if(!role) return message.reply(`<:erro:858615784771551252>| Você precisa mencionar ou usar o id de um cargo invalido!`);
 
 dbref.set({cargoId: role.id})
 
-message.reply(`☑️| Cargo setado com sucesso! Use \`${dbP.val().prefix}addvip [@mencão]\` para adicionar o VIP Em algum membro!`)
+message.reply(`☑️| Cargo setado com sucesso! Use \`${prefix}addvip [@mencão]\` para adicionar o VIP Em algum membro!`)
 }
