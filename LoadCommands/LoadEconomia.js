7cb21fca1e908cb2f14dfeb9c22f66bdbf9b6c68
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-18 02:22:29 -0300

    update LoadEconomia.js

--- LoadCommands/LoadEconomia.js
+++ LoadCommands/LoadEconomia.js
@@ -3,23 +3,23 @@ const fs = require("fs")
 const { join } = require("path")
 module.exports = async (client) => {
 let dir = __dirname
+
 client.economia = new Discord.Collection();
-  
 const FireSimple = require("../DatabaseUtil.js");
   client.db = new FireSimple({
     apiKey: "AIzaSyAgv6EICjfRgQJPTvsEJPjTPwYl_Rq2d5U",
     databaseURL: "https://royale-bot-7b344-default-rtdb.firebaseio.com"
   });
-
+  
   const cmds = fs.readdirSync(join(__dirname, "../Economia")).filter(file => file.endsWith(".js"));
   for(let file of cmds) {
-    let economia = require(join(__dirname, "../Economia", `${file}`));
-    client.economia.set(`${file}`.replace(".js", ""), economia);
-    if(cmd.conf && cmd.conf.aliases) {
-      cmd.conf.aliases.forEach(alias => {
-        client.economia.set(alias, economia);
+    let eco = require(join(__dirname, "../Economia", `${file}`));
+    client.economia.set(`${file}`.replace(".js", ""), eco);
+    if(ave.conf && ave.conf.aliases) {
+      ave.conf.aliases.forEach(alias => {
+        client.economia.set(alias, eco);
       })
     }
   }
-console.log("Economia carregada com sucesso!")
+console.log("Economia carregada com sucesso!");
 }
