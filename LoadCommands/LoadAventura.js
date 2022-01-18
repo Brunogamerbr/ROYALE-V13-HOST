798ea3c5f0fc1ffdeb4081782978de55e2074bc9
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-18 01:21:24 -0300

    update LoadAventura.js

--- LoadCommands/LoadAventura.js
+++ LoadCommands/LoadAventura.js
@@ -5,15 +5,15 @@ module.exports = async (client) => {
 let dir = __dirname
 client.commands = new Discord.Collection();
   
-const FireSimple = require("./DatabaseUtil.js");
+const FireSimple = require("../DatabaseUtil.js");
   client.db = new FireSimple({
     apiKey: "AIzaSyAgv6EICjfRgQJPTvsEJPjTPwYl_Rq2d5U",
     databaseURL: "https://royale-bot-7b344-default-rtdb.firebaseio.com"
   });
 
-  const cmds = fs.readdirSync(join(__dirname, "./Aventura")).filter(file => file.endsWith(".js"));
+  const cmds = fs.readdirSync(join(__dirname, "../Aventura")).filter(file => file.endsWith(".js"));
   for(let file of cmds) {
-    let ave = require(join(__dirname, "./Aventura", `${file}`));
+    let ave = require(join(__dirname, "../Aventura", `${file}`));
     client.commands.set(`${file}`.replace(".js", ""), ave);
     if(ave.conf && ave.conf.aliases) {
       ave.conf.aliases.forEach(alias => {
