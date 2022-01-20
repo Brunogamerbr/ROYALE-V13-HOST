a6ae6efd86ba01ae0944ad051a76a01ec4a22dfe
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-20 03:38:01 -0300

    update say.js

--- Moderação/say.js
+++ Moderação/say.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args, database) => {
 if (!message.member.permission.has(Permissions.FLAGS.ADMINISTRATOR)) { return message.reply('<:erro:858615784771551252>| Você não pode usar esse comando!') }
 
 if(!args[0]) {
-  message.reply(`Escreva uma mensagem após o Comando!`)
+message.reply(`Escreva uma mensagem após o Comando!`)
 return;
 }
 const say = args.join(' ')
