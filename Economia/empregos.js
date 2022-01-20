d3ec83b7fec13c9df180472e93c3e28bf3be7018
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-20 10:20:16 -0300

    update empregos.js

--- Economia/empregos.js
+++ Economia/empregos.js
@@ -1,86 +1,60 @@
 const Discord = require("discord.js") // Puxando a livraria Discord.js
 
-exports.run = async (client, message, args, database, prefix) => {
+module.exports.run = async (client, message, args, database, prefix) => {
 
- 
-  
-  
-
-
-let user1 = message.author
-
-
-
-  
-
-
-
-
-
-  let dbs = await   
-database.ref(`Start/${user1.id}`).once('value');
+ let user1 = message.author
+  let dbs = await database.ref(`Start/${user1.id}`).once('value');
   let dbsref = database.ref(`Start/${user1.id}`);
-
-  let db1 = await   
-database.ref(`Versao/${user1.id}`).once('value');
+  let db1 = await database.ref(`Versao/${user1.id}`).once('value');
   let db1ref = database.ref(`Versao/${user1.id}`);
-
-  let db2 = await   
-database.ref(`VersaoBuild`).once('value');
+  let db2 = await database.ref(`VersaoBuild`).once('value');
   let d2ref = database.ref(`VersaoBuild`);
 
   if (dbs.val() == null) {
-  message.inlineReply(`**Antes de começar a usar minha Economia você deve usar \`${prefix}start\` Para liberar meus comandos de Economia**`)
-return;
-    }
+return message.reply(`**Antes de começar a usar minha Economia você deve usar \`${prefix}start\` Para liberar meus comandos de Economia**`)}
 
-  
+
 if (db1.val().versão !== db2.val().build) {
-message.inlineReply(`**Tem uma nova Versão Disponível para sua Conta. Use \`${prefix}update\` Para aproveitar a nova Versão. Para mais informações entre em meu Servidor de Suporte \`${prefix}invite\`**`)
-return;
+return message.reply(`**Tem uma nova Versão Disponível para sua Conta. Use \`${prefix}update\` Para aproveitar a nova Versão. Para mais informações entre em meu Servidor de Suporte \`${prefix}invite\`**`)
 }
   
-  
-  
   let user = message.author
-
-
-
-
   let db = await database.ref(`Empregos/${user.id}`).once('value');
   let dbref = database.ref(`Empregos/${user.id}`);
   
 
+  if (db.val().emprego == 1) return message.reply(`Você ja possui um empredo de: \`💻 Programador\`, Você deve pedir demissão para visitar a Agência!`)
+  
+  if (db.val().emprego == 2) return message.reply(`**Você ja possui um empredo de: \`⛏️ Minerador\`, Você deve pedir demissão Para visitar a Agência!`)
 
-
-
-  if (db.val().emprego == 1) return message.inlineReply(`**Você ja possui um empredo de: \`💻 Programador\`, Você deve pedir demissão para visitar a Agência.**`) // Se o usuario tiver 1 na db no caso emprego de programador ele retornara esta mensagem.
-  if (db.val().emprego == 2) return message.inlineReply(`**Você ja possui um empredo de: \`⛏️ Minerador\`, Você deve pedir demissão Para visitar a Agência.**`) // Se o usuario tiver 1 na db no caso emprego de minerador ele retornara esta mensagem.
-
-  let embed = new Discord.MessageEmbed() // Criando uma embed
+  let embed = new Discord.MessageEmbed() 
     .setAuthor(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
     .setDescription(`
    **Olá ${user} sejá bem-vindo(a) a Nossa agencia de Empregos, veja abaixo todos os Empregos disponíveis:**\n**
 💻<a:seta:882102474264154215> Programador**\n**⛏️<a:seta:882102474264154215> Minerador**\n`)
-.setThumbnail(`https://i.imgur.com/HkIASTr.png`)
-.setFooter("Reaja com o emoji referente ao Emprego desejado")
-.setColor(`0D02FA`)
+    .setThumbnail(`https://i.imgur.com/HkIASTr.png`)
+    .setFooter("Reaja com o emoji referente ao Emprego desejado")
+    .setColor(`0D02FA`)
   
-  let m = await message.inlineReply(embed).then(async (m) => {
-    m.react('💻').then(() => m.react('⛏️'));
+  const m = await message.reply({embeds: [embed})
+     m.react('⛏️'); m.react("💻")
+let filter = (reaction, user) => {
+	return reaction.emoji.name === '⛏️' && user.id === message.author.id;
+};
+let collector = m.createReactionCollector({ filter, time: 15000 });
+collector.on('collect', (reaction, user) => {
+          dbref.update({emprego: 1})
+        message.channel.send('**☑️| Parabéns! Agora você trabalhará como um 💻 Programador**');
+})
 
-    let c1 = await m.createReactionCollector((r, u) => u.id === user.id, {max: 1, time: 60000}).on('collect', async (reaction, user) => {
-      if(reaction.emoji.name == '💻') { // Caso o usuário clique no emoji referente à Programador
-          dbref.update({ emprego: db.val().emprego + 1 })
-        return message.channel.send('**☑️| Parabéns! Agora você trabalhará como um 💻 Programador**');
-
-          // iremos adicionar 1 (um) na DB, que iremos usar como Programador
-      } else if(reaction.emoji.name == '⛏️') {
-        dbref.update({ emprego: db.val().emprego + 2 })
-        return message.channel.send('**☑️| Parabéns! Agora você trabalhará como um ⛏️ Minerador**')
-      } else return message.reply('**O tempo acabou. Utilize o comando novamente para poder escolher um emprego.**');
-    })
-  })
+let filter = (reaction, user) => {
+	return reaction.emoji.name === '💻' && user.id === message.author.id;
+};
+let collector = m.createReactionCollector({ filter, time: 15000 });
+collector.on('collect', (reaction, user) => {
+        dbref.update({emprego: 2})
+       message.channel.send('**☑️| Parabéns! Agora você trabalhará como um ⛏️ Minerador**')
+})
 }
 
 exports.conf = {
