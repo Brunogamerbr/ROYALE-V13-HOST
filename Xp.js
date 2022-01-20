b769b663c4e6e44de1ca9db91a8c8e669207b686
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-20 03:56:12 -0300

    Create Xp.js

---
+++ Xp.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+
+module.exports = async(client, message, database) => {
+
+      /*====== XP ==========*/
+  let dbE = await client.db.get(`Economia/${message.author.id}`)
+  let dbN = await client.db.get(`Nivel/${message.author.id}`)
+  if(dbN == null) {
+client.db.set(`Nivel/${message.author.id}`, {xp: 0}, {nivel: 1}); 
+  } else {
+      
+client.db.set(`Nivel/${message.author.id}`, {xp: dbN.xp + Math.floor(Math.random() * (15 - 3)) + 3}); 
+if(dbN.level * 340 <= dbN.xp) {
+           
+client.db.set(`Nivel/${message.author.id}`, {xp: 0}, {nivel: dbN.nivel + 1}); 
+}}
+
+
+let db = await client.db.get(`StartRPG/${message.author.id}`)
+let db1 = await client.db.get(`InventarioRPG/${message.author.id}`)
+if (db == null) return;
+if (db.hp == null) db.hp = 8;
+if (db.hp <0 || db.hp == 0){
+return message.reply(`${message.author} Você acabou perdendo todos os corações de **HP** você morreu... mas tente iníciar uma nova jornada!`)
+	await client.db.remove(`StartRPG/${message.author.id}`)
+
+await client.db.remove(`InventarioRPG/${message.author.id}`)	
+	await client.db.set(`StartRPG/${message.author.id}`)
+}
+}
