77de04c463fe35264cd33830a26dad552cc32fe8
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-20 03:25:51 -0300

    update setprefix.js

--- Moderação/setprefix.js
+++ Moderação/setprefix.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js")
+const { Permissions } = require('discord.js');
 module.exports.run = async(client, message, args, database, prefix) => {
-      
 	    let dbref = database.ref(`Servidores/${message.guild.id}`);
 	    let db = await database.ref(`Servidores/${message.guild.id}`).once('value');
       
     let prefix1 = args[0]
-      if(!message.member.hasPermission("MANAGE_GUILD")) return message.inlineReply(`:x: **|** Você precisa da permissão \` GERENCIAR SERVIDOR \` para poder usar esse comando.`);
+      if(!message.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) return message.reply(`<:erro:858615784771551252>**|** Você precisa da permissão \` GERENCIAR SERVIDOR \` para poder usar esse comando!`);
 
-      if(!prefix) return message.inlineReply(`:x: **|** Não é possível deixar o prefixo em branco!`);
+      if(!prefix) return message.reply(`<:erro:858615784771551252>**|** Não é possível deixar o prefixo em branco!`);
 
       if(!db.val()) {
         dbref.set({
@@ -18,6 +18,5 @@ module.exports.run = async(client, message, args, database, prefix) => {
           prefix: prefix1.toLowerCase()
         })
       }
-
-      message.inlineReply(`✅ **|** Prefixo alterado com sucesso para \` ${args[0]} \`!`)
+message.reply(`☑️*|** Prefixo alterado com sucesso para \` ${args[0]} \`!`)
 }
