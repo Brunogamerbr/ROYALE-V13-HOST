6f2841efbc0559c2b5a52351206152ebdbaeda2c
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-20 03:06:42 -0300

    update kick.js

--- Moderação/kick.js
+++ Moderação/kick.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const { MessageEmbed } = require('discord.js');
 module.exports.run = async (client, message, args, database, prefix) => {
 
   if(!message.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS)) return message.reply(`<:erro:858615784771551252> | ${message.author} Você precisa da permissão **EXPULSAR MEMBROS** para utilizar este comando!`)
