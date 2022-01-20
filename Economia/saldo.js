a65bdf6a2bf363e758ab4060b6304ebca2a26ab2
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-20 10:07:13 -0300

    update saldo.js

--- Economia/saldo.js
+++ Economia/saldo.js
@@ -1,61 +1,37 @@
 const Discord = require("discord.js");
+const { MessageEmbed } = require('discord.js');
+module.exports.run = async(client, message, args, database, prefix) => {
 
-exports.run = async(client, message, args, database, prefix) => {
-
-let user1 = message.author
- 
-  
-  let dbs = await   
-database.ref(`Start/${user1.id}`).once('value');
+  let user1 = message.author
+  let dbs = await database.ref(`Start/${user1.id}`).once('value');
   let dbsref = database.ref(`Start/${user1.id}`);
-
-  let db1 = await   
-database.ref(`Versao/${user1.id}`).once('value');
+  let db1 = await database.ref(`Versao/${user1.id}`).once('value');
   let db1ref = database.ref(`Versao/${user1.id}`);
-
-  let db2 = await   
-database.ref(`VersaoBuild`).once('value');
+  let db2 = await database.ref(`VersaoBuild`).once('value');
   let d2ref = database.ref(`VersaoBuild`);
 
-  if (dbs.val() == null) {
-  message.inlineReply(`**Antes de começar a usar minha Economia você deve usar \`${prefix}start\` Para liberar meus comandos de Economia**`)
-return;
-    }
-
+  if(dbs.val() == null) {
+ return message.reply(`**Antes de começar a usar minha Economia você deve usar \`${prefix}start\` Para liberar meus comandos de Economia**`)}
   
-if (db1.val().versão !== db2.val().build) {
-message.inlineReply(`**Tem uma nova Versão Disponível para sua Conta. Use \`${prefix}update\` Para aproveitar a nova Versão. Para mais informações entre em meu Servidor de Suporte \`${prefix}invite\`**`)
-return;
-  }
+  if(db1.val().versão !== db2.val().build) {
+return message.reply(`**Tem uma nova Versão Disponível para sua Conta. Use \`${prefix}update\` Para aproveitar a nova Versão. Para mais informações entre em meu Servidor de Suporte \`${prefix}invite\`**`)
+}
 
+  let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author 
 
-  
-
-let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author 
-
-
-let dbu = await   
-database.ref(`Start/${user.id}`).once('value');
+  let dbu = await database.ref(`Start/${user.id}`).once('value');
   let dburef = database.ref(`Start/${user.id}`);
   
   if (dbu.val() == null) {
- message.inlineReply(`**O usuário(a) ${user} Não está em meu banco de dados**`)
-return;
-  }
+ return message.reply(`**O usuário(a) ${user} Não está em meu banco de dados**`)}
   
 
   let db = await database.ref(`Economia/${user.id}`).once('value');
   let dbref = database.ref(`Economia/${user.id}`);
-
   let dbc = await database.ref(`Banco/${user.id}`).once('value');
   let dbcref = database.ref(`Banco/${user.id}`);
 
 
-      
-  
-
-  
-  
   let embed = new Discord.MessageEmbed()
     .setColor("#0D02FA")
     .setTitle("<:royalecoin:882701325438156801>**|** Balanço Monetário")
@@ -65,10 +41,9 @@ return;
     .setFooter("Informações da sua carteira!")
     .setThumbnail('https://i.imgur.com/K33aRIu.png')
     .setTimestamp()
-  message.inlineReply(embed)
-	
+  message.reply({embeds: [embed]})
 }
   
 exports.conf = {
   aliases: ["coins", "bal", "money", "atm", "balance", "dinheiro"]
-}  
+}
