062e3842ba0851105641650a5bf2d7fba8af6892
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-20 03:39:24 -0300

    update say.js

--- Moderação/say.js
+++ Moderação/say.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 const { Permissions } = require('discord.js');
 module.exports.run = async (client, message, args, database) => {
-if (!message.member.permission.has(Permissions.FLAGS.ADMINISTRATOR)) { return message.reply('<:erro:858615784771551252>| Você não pode usar esse comando!') }
+if (!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) { return message.reply('<:erro:858615784771551252>| Você não pode usar esse comando!') }
 
 if(!args[0]) {
 message.reply(`Escreva uma mensagem após o Comando!`)
