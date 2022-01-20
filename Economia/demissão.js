d3016a2d8b8dd870262ee32526b4003d995ecd01
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-20 07:31:53 -0300

    update demissão.js

--- Economia/demissão.js
+++ Economia/demissão.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js")
 const { MessageEmbed } = require('discord.js');
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 module.exports.run = async (client, message, args,database, prefix) => {
 
   let user1 = message.author
