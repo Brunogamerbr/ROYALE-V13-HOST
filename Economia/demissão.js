bbac74a955caa70b150507f7c71fb54935d4bc6f
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-20 09:13:54 -0300

    update demissão.js

--- Economia/demissão.js
+++ Economia/demissão.js
@@ -44,7 +44,7 @@ const filter = (reaction, user) => {
 	return reaction.emoji.name === '👍' && user.id === message.author.id;
 };
 
-const collector = message.createReactionCollector({ filter, time: 15000 });
+const collector = pro.createReactionCollector({ filter, time: 15000 });
 
 collector.on('collect', (reaction, user) => {
 	console.log(`Collected ${reaction.emoji.name} from ${user.tag}`);
