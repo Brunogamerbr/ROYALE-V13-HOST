26194dd8686c955c1aed535d73a061706e53cdd3
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-20 17:45:05 -0300

    update demissão.js

--- Economia/demissão.js
+++ Economia/demissão.js
@@ -49,20 +49,18 @@ return;
 
 
    if(dbE.val().emprego == 2) {
-    let mine = await message.channel.send({content: `Você realmente deseja largar a vida de Minerador? essa ação custará R$3500 de sua carteira!`, fetchReply: true});
+   let mine = await message.channel.send({content: `Você realmente deseja largar a vida de Minerador? essa ação custará R$3500 de sua carteira!`, fetchReply: true});
     mine.react('☑️');
-    const filter1 = (reaction, user) => {
-  	return reaction.emoji.name === '☑️' && user.id === message.author.id;}
-  	
-   const collector1 = mine.createReactionCollector({ filter1, time: 15000 });
-   collector1.on('collect', (reaction, user) => {
+  const filterr = (reaction, user) => {
+	return reaction.emoji.name === '☑️' && user.id === message.author.id;
+}
+  const collectorr = pro.createReactionCollector({ filter, time: 15000 })
+  collectorr.on('collect', (reaction, user) => {  
 	 message.reply(`☑️| Você pediu demissão do emprego de minerador!`)
 	 dbEref.update({emprego: 0})
 	 dbref.update({dinheiro: db.val().dinheiro - 3500})
 });
-}
-}
-     
+}}
 exports.conf = {
   aliases: ["demissao"]
 }
