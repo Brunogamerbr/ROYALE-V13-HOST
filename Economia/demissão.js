f4d5b1e654b38f9051c6d5c59ec1ba82eca01a34
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-20 18:08:18 -0300

    update demissão.js

--- Economia/demissão.js
+++ Economia/demissão.js
@@ -32,7 +32,7 @@ module.exports.run = async(client, message, args,database, prefix) => {
   if(db.val() == null || db.val().dinheiro < 3500) return message.reply('**Para pedir demissão você necessita de R$3500 na sua Carteira!**') 
   
   
-  if (dbE.val().emprego == 1) {
+  /*if (dbE.val().emprego == 1) {
   let pro = await message.channel.send({content: `Você realmente deseja largar a vida de programador? essa ação custará R$3500 de sua carteira!`, fetchReply: true});
   pro.react('☑️');
   let filter = (reaction, user) => {
@@ -44,16 +44,15 @@ module.exports.run = async(client, message, args,database, prefix) => {
 	 dbref.update({dinheiro: db.val().dinheiro - 3500})
 	 return message.reply(`☑️| Você pediu demissão do emprego de programador!`)
 });
-}
+}*/
 
-
-   if(dbE.val().emprego == 2) {
+if(dbE.val().emprego == 2) {
    let mine = await message.channel.send({content: `Você realmente deseja largar a vida de Minerador? essa ação custará R$3500 de sua carteira!`, fetchReply: true});
     mine.react('☑️');
 
  let filter1 = (reaction, user) => {
 	return reaction.emoji.name === '☑️' && user.id === message.author.id;
-}
+};
   let collector1 = mine.createReactionCollector({ filter1, time: 15000 });
 
    collector1.on('collect', (reaction, user) => {
@@ -62,6 +61,7 @@ module.exports.run = async(client, message, args,database, prefix) => {
 	 return message.reply(`☑️| Você pediu demissão do emprego de minerador!`)
 })
 }
+   
 }
 exports.conf = {
   aliases: ["demissao"]
