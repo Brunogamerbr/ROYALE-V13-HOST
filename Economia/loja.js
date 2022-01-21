47a752402e05de3058f64ee4e1d71a16a3d89db4
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-21 11:52:03 -0300

    update loja.js

--- Economia/loja.js
+++ Economia/loja.js
@@ -78,13 +78,14 @@ const collector = m.createMessageComponentCollector({ filter, time: 15000 });
 collector.on('collect', async i => {
 	if (i.customId === 'PRIMARY1') {
 if(db.val().dinheiro < 7000){
-  setTimeout(function() {m.delete()}, 100);
+  setTimeout(function() {m.delete() message.delete()}, 100);
 return message.channel.send({content:`<:erro:858615784771551252>**|** ${message.author} Você não tem dinheiro suficiente para comprar um **Porte de Armas**`})
 }
 message.channel.send(`Você comprou um **Porte de armas** no valor de **R$7000**!`);
 dbref.update({dinheiro: db.val().dinheiro - 7000})
 dbLref.update({porte: 1})	
 m.delete()
+message.delete()
 }
 });
 collector.on('end', collected => {
