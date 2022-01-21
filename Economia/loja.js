0cea52282e4af1d462af2cb42540926174bfb834
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-21 12:39:38 -0300

    update loja.js

--- Economia/loja.js
+++ Economia/loja.js
@@ -109,6 +109,8 @@ if(db.val().dinheiro < 500){
 if(dbL.val().escolta_time != 0 && timeout - (Date.now() - dbL.val().escolta_time) > 0) {
    let time = ms(timeout - (Date.now() - dbL.val().escolta_time));
   return message.channel.send(`<:erro:858615784771551252>| Você ainda tem **${time.minutes}m ${time.seconds}** de **Escolta**, aguarde esse tempo acabar para comprar outra!`)
+  m.delete()
+  message.delete()
    }
 
 message.channel.send(`Você comprou um **1 hora de Escolta** no valor de **R$500**!`);
