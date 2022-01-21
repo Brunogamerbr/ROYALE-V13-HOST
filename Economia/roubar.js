9923d8a710d34d9dbc9882863179e798153a0bb3
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-21 14:39:58 -0300

    update roubar.js

--- Economia/roubar.js
+++ Economia/roubar.js
@@ -4,83 +4,78 @@ const ms = require("parse-ms");
 module.exports.run = async (client, message, args, database, prefix) => {
 let user = message.author;
 let cara = message.mentions.users.first();
-
 let db4 = await database.ref(`Start/${user.id}`).once('value');
 let db4ref = await database.ref(`Start/${user.id}`);
 
 
 if (db4.val() == null) {
-message.inlineReply(`**Antes de começar a usar minha Economia. você deve usar \`${prefix}start\` Para liberar meus comandos de Economia**`);
+message.reply(`**Antes de começar a usar minha Economia. você deve usar \`${prefix}start\` Para liberar meus comandos de Economia**`);
  return;
 }
 
 let db5 = await database.ref(`Versao/${user.id}`).once('value');  
 let db6 = await database.ref(`VersaoBuild`).once('value');
+let db1L = await database.ref(`Loja/${cara.id}`).once('value')
+let db2L = await database.ref(`Loja/${cara.id}`)
 
 if (db5.val().versão !== db6.val().build){
-message.inlineReply(`**Tem uma nova Versão Disponível para sua Conta. Use \`${prefix}update\` Para aproveitar a nova Versão. Para mais informações entre em meu Servidor de Suporte \`.invite\`**`)
+message.reply(`**Tem uma nova Versão Disponível para sua Conta. Use \`${prefix}update\` Para aproveitar a nova Versão. Para mais informações entre em meu Servidor de Suporte \`.invite\`**`)
 return;
 }
-  
 if (!cara) {
-message.inlineReply(`**<:erro:858615784771551252>| Mencione o usuário que você deseja Roubar.**`);
+message.reply(`<:erro:858615784771551252>| Mencione o usuário que você deseja Roubar!`);
 return; 
 }
-
 
 if (cara == message.author) {
 return message.inlineReply(`<:erro:858615784771551252>| Você não pode se roubar!`);
 }
 
-let db3 = await database.ref(`Start/${cara.id}`).once('value');        
+let db3 = await database.ref(`Start/${cara.id}`).once('value');  
 if (db3.val() == null) {
-message.inlineReply(`**<:erro:858615784771551252>| Esse usuário não está em minha Economia!**`)
+message.reply(`<:erro:858615784771551252>| Esse usuário não está em minha Economia!`)
   return;
 }
+if (db1L.val().escolta_time) {
+return message.reply(`<:erro:858615784771551252>| Esse usuário está com uma **escolta** ativa, você não poderá roubar ele!`)
+}
+  
   
 let db7 = await database.ref(`Versao/${user.id}`).once('value');  
 let db8 = await database.ref(`VersaoBuild`).once('value');
-
-  
 let db9 = await database.ref(`Economia/${cara.id}`).once('value');
 let db9ref = database.ref(`Economia/${cara.id}`); 
-  
 let db1 = await database.ref(`Loja/${user.id}`).once('value')
 let db2 = await database.ref(`Loja/${cara.id}`).once('value');
-  
+
+
+    
 if (!db1.val() || !db1.val().porte) {
-message.inlineReply(`**<:erro:858615784771551252>| Você não possui um Porte de Armas! compre um na loja usando \`.loja\`**`)
+message.reply(`<:erro:858615784771551252>| Você não possui um Porte de armas! compre um na loja usando \`.loja\`!`)
 return;
 }
 
 if (!db1.val() || !db1.val().pistola) {
-message.inlineReply(`**<:erro:858615784771551252>| Você não possui uma Pistola! compre uma na loja utilizando \`.loja\`**`)
+message.reply(`<:erro:858615784771551252>| Você não possui uma Pistola! compre uma na loja utilizando \`.loja\`!`)
 return;
 }
   
 if (db9.val().dinheiro <0) {
-message.inlineReply(`**<:erro:858615784771551252>| O usuário mencionado não possui saldo suficiente para ser roubado!**`)
+message.reply(`<:erro:858615784771551252>| O usuário mencionado não possui saldo suficiente para ser roubado!`)
 return;
 }
 
  if (db9.val().dinheiro == 0 ){
-message.inlineReply(`<:erro:858615784771551252>| O usuário mencionado Não tem saldo na Carteira para ser roubado.`)
+message.reply(`<:erro:858615784771551252>| O usuário mencionado Não tem saldo na Carteira para ser roubado.`)
 return;
  }
 
 let db = await database.ref(`Economia/${user.id}`).once('value');
 let dbref = database.ref(`Economia/${user.id}`);
-
 let roubo = ["n", "Preso", "n", "Preso", "Preso"]
-let caiu = roubo[Math.floor(Math.random() * roubo.length)] 
-  
- 
-    
+let caiu = roubo[Math.floor(Math.random() * roubo.length)]
 let timeout = 600000;
-
 let amount = Math.floor(Math.random() * 4000) + 1000;
-  
-
 let tb = await database.ref(`Transacoes/${message.author.id}`).once("value")
 let tbref = database.ref(`Transacoes/${message.author.id}`)
 let tb2 = await database.ref(`Transacoes/${cara.id}`).once("value")
@@ -91,7 +86,7 @@ let tbref2 = database.ref(`Transacoes/${cara.id}`)
 
   if(db.val().roubotime !== null && timeout - (Date.now() - db.val().roubotime) > 0) {
 let time = ms(timeout - (Date.now() - db.val().roubotime));
-return message.inlineReply(`**<:erro:858615784771551252>| Espere ${time.hours}h ${time.minutes}m ${time.seconds}s Para realizar um Roubo novamente!**`);
+return message.reply(`**<:erro:858615784771551252>| Espere ${time.hours}h ${time.minutes}m ${time.seconds}s Para realizar um Roubo novamente!**`);
 }
 
 
@@ -108,7 +103,7 @@ let embed = new Discord.MessageEmbed()
 .setFooter(`Roubo mal sucedido`)
 .setColor(`#0D02FA`)
 .setTimestamp()
-message.inlineReply(embed)                                  
+message.inlineReply({embeds: [embed]})                                  
 
 dbref.update({dinheiro: db.val().dinheiro - amount, roubotime: + Date.now()});
 return;
@@ -131,7 +126,7 @@ let embed2 = new Discord.MessageEmbed()
 .setFooter(`Roubo bem sucedido`)
 .setTimestamp()
 .setColor(`#0D02FA`)
-message.channel.send(embed2)
+message.channel.send({embeds: [embed2]})
 
 db9ref.update({dinheiro: db9.val().dinheiro - amount })
 dbref.update({dinheiro: db.val().dinheiro + amount, roubotime: Date.now()})
