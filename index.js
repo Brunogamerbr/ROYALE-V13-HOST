f659f8b2f59b3d5f829f7854b880f7aa52e34b86
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-22 19:07:05 -0300

    update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { join } = require("path");
 require("./RifaOn.js")(client)
 
 process.on('uncaughtException', (err) => {
-client.channels.cache.get("931443923862302761")
+let channel = client.channels.cache.get("931443923862302761")
 channel.send(`UM NOVO ERRO DETECTADO!\n\n\`\`\`js\n${err}\n\`\`\``)
 })
 
