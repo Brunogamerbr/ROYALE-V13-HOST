3cd6a515971d3f0116db0694293a58e3d9ee115c
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-22 16:01:17 -0300

    update gato.js

--- Diversão/gato.js
+++ Diversão/gato.js
@@ -1,1 +1,19 @@
-.
+const Discord = require("discord.js");
+const request = require('request');
+module.exports.conf = {
+  aliases: ["cat", "gatinho"]
+}
+
+module.exports.run = async function(client, message, args, database) {
+  let user = message.mentions.users.first();
+  request.get('https://api.thecatapi.com/v1/images/search?mime_types=png&limit=1', async (error, resp, body)  => {
+    if(error) return;
+    body = JSON.parse(body)
+    let embed = new Discord.MessageEmbed()
+      .setImage(body[0].url)
+      .setColor("#0D02FA")
+      .setDescription(`${message.author} Aqui está seu gatinho`)
+      .setURL(body[0].url)
+    message.channel.send({embeds: [embed]});
+  });
+}
