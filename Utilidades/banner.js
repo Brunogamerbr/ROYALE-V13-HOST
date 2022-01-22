02d37da380ca2533a1481612d9c9a1aada63ba01
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-21 21:51:40 -0300

    update banner.js

--- Utilidades/banner.js
+++ Utilidades/banner.js
@@ -1,8 +1,6 @@
 const axios = require('axios');
 const discord = require('discord.js')
-
-exports.run = async (client, message, args, database) => {
-	
+module.exports.run = async (client, message, args, database) => {
 const user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author
 	axios.get(`https://discord.com/api/users/${user.id}`, {
  headers: {
@@ -11,24 +9,19 @@ const user = message.mentions.users.first() || client.users.cache.get(args[0]) |
         })
         .then((res) => {
  const { banner, accent_color } = res.data;
-
- if(banner) {
+if(banner) {
 const extension = banner.startsWith('a_') ? '.gif' : '.png'
-                const url = `https://cdn.discordapp.com/banners/${user.id}/${banner}${extension}?size=4096`;
+const url = `https://cdn.discordapp.com/banners/${user.id}/${banner}${extension}?size=4096`;
 
-                const embed = new discord.MessageEmbed()
-                
-                .setDescription(`Clique [aqui](${url}) para baixar o banner!`)
-                .setImage(url)
-                .setColor(`#0D02FA`)
-
-                message.inlineReply(embed)
-
-            } else {
-                if(accent_color) {
-                    message.inlineReply(`**${user} Você Não tem um banner personalizado!**`)
-} else return message.inlineReply(`${user} Não tem banner nem cor personalizado!`)
+const embed = new discord.MessageEmbed()
+.setDescription(`Clique [aqui](${url}) para baixar o banner!`)
+.setImage(url)
+.setColor(`#0D02FA`)
+message.reply({embeds: [embed]})
+} else {
+ if(accent_color) {
+message.reply(`<:erro:858615784771551252>| Você Não tem um banner personalizado!`)
+} else return message.reply(`<:erro:858615784771551252>| Não tem banner nem cor personalizado!`)
  }
  })
-    
 }
