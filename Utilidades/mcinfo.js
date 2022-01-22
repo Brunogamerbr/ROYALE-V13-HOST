8960a660c7522c26edbf782f168e92402b95829a
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-22 01:51:03 -0300

    update mcinfo.js

--- Utilidades/mcinfo.js
+++ Utilidades/mcinfo.js
@@ -1,23 +1,21 @@
 const Discord = require("discord.js");
 const request = require("request");
-
-exports.run = async function(client, message, args, database) {
-  if(!args.length) return message.inlineReply(`:x: **| Por favor, insira o nick de algum jogador de minecraft!**`);
+module.exports.run = async(client, message, args, database) => {
+  if(!args.length) return message.reply(`:x: **| Por favor, insira o nick de algum jogador de minecraft!**`);
   
   let playername = args[0];
-
   let uuid = "";
   let nicks = [];
 
   request.get(`https://api.mojang.com/users/profiles/minecraft/${playername}`, (err, res, body) => {
 
-    if(res.statusCode == 400) return message.inlineReply(`:x: **| O nome é inválido, tem mais de 16 caracteres ou contém caracteres diferentes de (a-zA-Z0-9_)**`);
+    if(res.statusCode == 400) return message.reply(`:x: **| O nome é inválido, tem mais de 16 caracteres ou contém caracteres diferentes de (a-zA-Z0-9_)**`);
 
-    if(res.statusCode == 429) return message.inlineReply(`:x: **| Muitos pedidos enviados**`);
+    if(res.statusCode == 429) return message.reply(`:x: **| Muitos pedidos enviados**`);
 
-    if(res.statusCode == 500) return message.inlineReply(`:x: **| Tempo limite esgotado (API atrasou e não pôde responder)**`);
+    if(res.statusCode == 500) return message.reply(`:x: **| Tempo limite esgotado (API atrasou e não pôde responder)**`);
 
-    if(res.statusCode != 200 || !body) return message.inlineReply(`:x: **| Não foi possível localizar o jogador (Verifique se o nome está correto)**`);
+    if(res.statusCode != 200 || !body) return message.reply(`:x: **| Não foi possível localizar o jogador (Verifique se o nome está correto)**`);
 
     
 
@@ -37,7 +35,7 @@ exports.run = async function(client, message, args, database) {
       \`\`\` ${nicks.map(n => n.name).join(" | ")} \`\`\`**`)
       .setImage(`https://mc-heads.net/head/${playername.name}`)
       .setAuthor(playername.name, `https://mc-heads.net/avatar/${playername.name}`);
-    message.inlineReply(embed);
+    message.reply({embeds: [embed]});
   }
 }
 
