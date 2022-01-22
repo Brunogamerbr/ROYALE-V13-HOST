5d0e0321db21b92746c61d9ffd3907eea750e124
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-22 12:02:21 -0300

    update block.js

--- Moderação/block.js
+++ Moderação/block.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const { Permissions } = require('discord.js');
 module.exports.run = async (client, message, args, database, prefix) => {
 
-if (!message.member.permissions.has(Permission.FLAGS.MANAGE_CHANNELS)) { return message.reply('<:erro:858615784771551252>| Você não tem permissão para usar esse comando!') }
+if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS)) { return message.reply('<:erro:858615784771551252>| Você não tem permissão para usar esse comando!') }
 
 let canal = message.channel.id;
 let block = await client.db.get(`Servidores_${message.guild.id}`);
