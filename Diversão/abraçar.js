afd789147dbaae4a9113d1d85b99b2abbe4d3d02
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-22 15:28:12 -0300

    update abraçar.js

--- Diversão/abraçar.js
+++ Diversão/abraçar.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const request = require('request');
-
 module.exports.run = async function(client, message, args, database) {
   let user = message.mentions.users.first();
   if(!user) return message.channel.send(`:x: **| Mencione alguém para abraçar.**`);
@@ -13,7 +12,7 @@ module.exports.run = async function(client, message, args, database) {
       .setColor("#0D02FA")
       .setDescription(`${message.author} Abraçou ${user}`)
       .setURL(body.url)
-    let msg = await message.channel.send(embed);
+    let msg = await message.channel.send({embeds: [embed]});
     start(msg);
   });
 
@@ -31,7 +30,7 @@ module.exports.run = async function(client, message, args, database) {
             .setColor("#0D02FA")
             .setDescription(`${user} Abraçou ${message.author} também`)
             .setURL(body.url)
-          message.channel.send(embed);
+          message.channel.send({embeds: [embed]});
         });
       }
     })
