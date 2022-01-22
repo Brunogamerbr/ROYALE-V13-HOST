c1351e12e9bc4e8330cd4c3fa3376d97ea4916a8
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-22 16:50:29 -0300

    Create help.js

---
+++ Utilidades/help.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js")
+exports.run = async (client, message, args) => {
+
+let embed = new Discord.MessageEmbed()
+ .setTitle(`**<:file:873563451665055754>| lista de comandos**`)  
+ .setDescription(`**Veja abaixo uma lista detalhada com todos os meus comandos disponíveis:\n\n<a:seta:882102474264154215>| ECONOMIA
+\`daily rank work time crime roll rinha rifa coinflip roubar sorte loja empregos depositar sacar pay saldo demissão transacoes\`\n\n<a:seta:882102474264154215>| UTILIDADES
+\`donate sugest banner bug invite userinfo afk ping avatar uptime clima\`\n\n<a:seta:882102474264154215>| MODERAÇÃO
+\`ban unban kick block setprefix anuncio sorteio setsugest addemoji addvip addcargo say clear\`\n\n<a:seta:882102474264154215>| DIVERSÃO
+\`cafune tapa abraço beijar ship sikera conquista bolsonaro laranjo mcinfo mchead mcbody pato gato cachorro passarinho coelho escanor\`\n\n<a:seta:882102474264154215>| AVENTURA:
+\`create-avatar open battle explorar chop eat buy shop bag av use\`\n\nGostou de mim? então me adicione em seu servidor clicando [Aqui](https://discord.com/api/oauth2/authorize?client_id=844227895819894794&permissions=287636319703&scope=bot)\n\nQuer me ajudar a crescer mais rápido e poder entreter mais pessoas? Vote em mim basta clicar [Aqui](https://top.gg/bot/844227895819894794)\n\nQuer ficar Atualizado sobre Novos comandos e novas atualização? Entre em Meu [Servidor de Suporte](https://discord.gg/u65cgdFHSN)**`)
+.setImage('https://i.imgur.com/Pg9AXWG.gif')
+.setThumbnail(`https://i.imgur.com/y3I6uCc.png`)
+.setFooter(`Mostrando meu Menu de Comandos`)
+.setColor(`#0D02FA`)
+.setTimestamp()
+message.reply({embeds: [embed]});
+}
+exports.conf = {
+  aliases: ["ajuda", "comandos"]
+}
