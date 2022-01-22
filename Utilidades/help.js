4ea21fe94bd4a61c1167d6d9274a76487494a6be
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-22 17:38:05 -0300

    update help.js

--- Utilidades/help.js
+++ Utilidades/help.js
@@ -5,7 +5,7 @@ let embed = new Discord.MessageEmbed()
  .setTitle(`<:file:873563451665055754>| lista de comandos`)  
  .setDescription(`Veja abaixo uma lista detalhada com todos os meus comandos disponíveis:\n\n**<a:seta:882102474264154215>| ECONOMIA
 \`daily rank work time crime roll rinha rifa coinflip roubar sorte loja empregos depositar sacar pay saldo demissão transacoes\`**\n\n**<a:seta:882102474264154215>| UTILIDADES
-\`donate sugest banner bug invite userinfo afk ping avatar uptime clima\`**\n\n**<a:seta:882102474264154215>| MODERAÇÃO
+\`donate sugest banner bug invite userinfo afk ping avatar uptime clima mcbody mcinfo mchead\`**\n\n**<a:seta:882102474264154215>| MODERAÇÃO
 \`ban unban kick block setprefix anuncio sorteio setsugest addemoji addvip addcargo say clear\`**\n\n**<a:seta:882102474264154215>| AVENTURA:
 \`create-avatar open battle explorar chop eat buy shop bag av use\`**\n\nGostou de mim? então me adicione em seu servidor clicando **[aqui](https://discord.com/api/oauth2/authorize?client_id=844227895819894794&permissions=287636319703&scope=bot)**`)
 .setImage('https://i.imgur.com/Pg9AXWG.gif')
