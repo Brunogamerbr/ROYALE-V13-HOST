83a671f75cfcd0354b1825163c82bf3c4429872d
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-22 16:35:40 -0300

    update abraçar.js

--- Diversão/abraçar.js
+++ Diversão/abraçar.js
@@ -1,38 +1,33 @@
 const Discord = require("discord.js");
 const request = require('request');
 module.exports.run = async (client, message, args, database) => {
-  let user = message.mentions.users.first();
+  const user = message.mentions.users.first();
   if(!user) return message.channel.send(`<:erro:858615784771551252>| Mencione alguém para abraçar!`);
-  request.get('https://nekos.life/api/v2/img/hug', async (error, resp, body)  => {
-    if(error) return;
-    body = JSON.parse(body)
-    let embed = new Discord.MessageEmbed()
+  const embed = new Discord.MessageEmbed()
       .setImage(body.url)
       .setColor("#0D02FA")
       .setDescription(`${message.author} Abraçou ${user}`)
       .setURL(body.url)
+  request.get('https://nekos.life/api/v2/img/hug', async (error, resp, body)  => {
+    if(error) return;
+    body = JSON.parse(body)
     const msg = await message.channel.send({embeds: [embed]});
-  
-    await msg.react("🔄");
-    let c1 = msg.createReactionCollector((r, u) => r.message.authot.id === u.user.id, {time: 60000});
-    c1.on("collect", (reaction, user) => {
-      if(reaction.emoji.name === "🔄") {
-   
-        request.get('https://nekos.life/api/v2/img/hug', async (error, resp, body)  => {
-          if(error) return;
-          body = JSON.parse(body)
-          let embed = new Discord.MessageEmbed()
+  })
+  const filter = (reaction, user) => {
+	return reaction.emoji.name === '💻' && user.id};
+  const collector = msg.createReactionCollector({ filter, time: 15000 });
+collector.on('collect', (reaction, user) => {
+  let embed = new Discord.MessageEmbed()
             .setImage(body.url)
             .setColor("#0D02FA")
             .setDescription(`${user} Abraçou ${message.author} também`)
             .setURL(body.url)
           message.channel.send({embeds: [embed]});
-        });
-      }
-    })
-  });
-  }
+})
+          
+
   
+}
 module.exports.conf = {
   aliases: ["hug", "abraçar", "abracar"]
 }
