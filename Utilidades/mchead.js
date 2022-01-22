e1e765bea73d729d08bfa78a3766b8845cce31c9
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-22 01:36:44 -0300

    update mchead.js

--- Utilidades/mchead.js
+++ Utilidades/mchead.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = async function(client, message, args, database) {
-  if(!args.length) return message.inlineReply(`:x: **| Por favor, insira o nick de algum jogador de minecraft!**`);
+  if(!args.length) return message.reply(`:x: **| Por favor, insira o nick de algum jogador de minecraft!**`);
   let embed = new Discord.MessageEmbed()
     .setDescription(`Cabeça de ${args[0]}`)
     .setImage(`https://mc-heads.net/head/${args[0]}`)
