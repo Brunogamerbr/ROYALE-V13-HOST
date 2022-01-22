5e08aeda5014645c59878836236e83616a08972f
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-21 21:47:13 -0300

    update afk.js

--- Utilidades/afk.js
+++ Utilidades/afk.js
@@ -1,14 +1,12 @@
 const Discord = require('discord.js');
 const db = require('quick.db');
 
-exports.run = async (client, message, args, database) => {
+module.exports.run = async (client, message, args, database) => {
 const status = new db.table("AFKs");
 let afk = await status.fetch(message.author.id);
-
 status.set(message.author.id, args.join(" ") || `AFK`);
-
   if (!afk)  {
-    message.inlineReply(`${message.author} modo AFK ativado\n Motivo: **\`${args.join(" ") ? args.join(" ") : "não definido"}\`**`);
+    message.reply(`${message.author} modo AFK ativado\n Motivo: **\`${args.join(" ") ? args.join(" ") : "não definido"}\`**`);
   } else {
     status.delete(message.author.id);
   }
