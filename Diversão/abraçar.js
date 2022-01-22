3a8af2b3176b9b688594fc5fc5887f59281ac3d9
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-22 16:09:21 -0300

    update abraçar.js

--- Diversão/abraçar.js
+++ Diversão/abraçar.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const request = require('request');
-module.exports.run = async function(client, message, args, database) {
+module.exports.run = async (client, message, args, database) => {
   let user = message.mentions.users.first();
   if(!user) return message.channel.send(`<:erro:858615784771551252>| Mencione alguém para abraçar!`);
   request.get('https://nekos.life/api/v2/img/hug', async (error, resp, body)  => {
@@ -12,15 +12,14 @@ module.exports.run = async function(client, message, args, database) {
       .setDescription(`${message.author} Abraçou ${user}`)
       .setURL(body.url)
     let msg = await message.channel.send({embeds: [embed]});
-    start(msg);
+    
   });
-
-  async function start(msg) {
+  
     await msg.react("🔄");
     let c1 = msg.createReactionCollector((r, u) => u.id === user.id, {time: 60000});
     c1.on("collect", (reaction, user) => {
       if(reaction.emoji.name === "🔄") {
-        c1.stop();
+   
         request.get('https://nekos.life/api/v2/img/hug', async (error, resp, body)  => {
           if(error) return;
           body = JSON.parse(body)
