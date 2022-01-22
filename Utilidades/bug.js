64dd5ca4ac5e6f6bc50e70755d22c600250d01c0
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-21 23:54:49 -0300

    update bug.js

--- Utilidades/bug.js
+++ Utilidades/bug.js
@@ -1,27 +1,25 @@
 const Discord = require("discord.js")
-exports.run = async (client, message, args, database) => {
-  
+module.exports.run = async (client, message, args, database) => {
 var doggo = message.guild.members.cache.get(client.user.id);
 
-if(!doggo.hasPermission('MANAGE_MESSAGES')){
-return message.inlineReply(`<:erro:858615784771551252>| Está me faltando permissão de \`gerenciar mensagens\` nesse servidor!`)}
+if(!doggo.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)){
+return message.reply(`<:erro:858615784771551252>| Está me faltando permissão de \`gerenciar mensagens\` nesse servidor!`)}
 
 let content = args.join(` `)
 let user = message.author
 
 if (!args[0]) {
-return message.inlineReply(`<:erro:858615784771551252>| Escreva seu problema após o comando!`)
+return message.reply(`<:erro:858615784771551252>| Escreva seu problema após o comando!`)
 }
 
 let canal = client.channels.cache.get("916823927853051955");
-
 let embed = new Discord.MessageEmbed()  
 .setTitle(`**Um novo bug foi Reportado**`)
 .setDescription(`**Bug reportado por: \`${message.author.tag}\`\nID: \`${user.id}\`\nBUG: \`${content}\`**`)
   .setColor(`#0D02FA`)
 canal.send(embed)
 message.delete()
-await message.reply(`**☑️| Sua Reportagem de Bug foi enviada para a Minha equipe Analisar!**`)
+await message.reply(`☑️| Seu report foi enviado até a minha equipe! tentaremos resolver seu problema o mais rápido possível!`)
 }
 exports.conf = {
   aliases: ["report", "reportar"]
