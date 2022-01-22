a05571b34e37e81cf10828643517a5c2679e7922
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-22 15:33:18 -0300

    Create clima.js

---
+++ Utilidades/clima.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const weather = require('weather-js');
+
+
+module.exports.run = async function(client, message, args, database) {
+  if(!args.length) return message.inlineReply(":x: **| Por favor, insira uma localização válida!**");
+
+
+  weather.find({ search: args.join(' '), degreeType: 'C' }, async(error, result) => {
+    if(error) return message.inlineReply(`:x: **| Um erro interno aconteceu. Tente novamente mais tarde!**`);
+    if(result == undefined || result.length == 0) {
+        return message.inlineReply(':x: **| Local não encontrado!**');
+    }
+
+    var current = result[0].current;
+    var location = result[0].location;
+
+    let embed = new Discord.MessageEmbed()
+      .setAuthor(`Previsão do tempo para ${current.observationpoint}`)
+      .setColor("#0D02FA")
+      .addField(`Fuso Horário`, `UTC${location.timezone}`, true)
+      .addField(`Temperatura`, `${current.temperature}°C`, true)
+      .addField(`Sensação Térmica`, `${current.feelslike}°C`, true)
+      .addField(`Velocidade do vento`, `${current.winddisplay}`, true)
+      .addField('Umidade do ar', `${current.humidity}%`, true)
+    message.inlineReply(embed)
+  })
+
+}
