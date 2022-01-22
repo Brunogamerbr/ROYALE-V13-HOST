8f3d0e0b1da7a8675a95ec274807dbd786c2f1f5
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-22 15:53:20 -0300

    update index.js

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ require("./Handlers/HandlerAventura.js")(client, message, database, config);
 require("./Handlers/HandlerModeração.js")(client, message, database, config);
 require("./Handlers/HandlerDevelopers.js")(client, message, database, config);
 require("./Handlers/HandlerUtilidades.js")(client, message, database, config);
+require("./Handlers/HandlerDiversão.js")(client, message, database, config);
 require("./Afk.js")(client, message, database, config);
 });
 
@@ -36,6 +37,7 @@ require("./LoadCommands/LoadModeração.js")(client);
 require("./LoadCommands/LoadEconomia.js")(client);
 require("./LoadCommands/LoadDevelopers.js")(client);
 require("./LoadCommands/LoadUtilidades.js")(client);
+require("./LoadCommands/LoadDiversão.js")(client);
 });
 
 process.on('uncaughtException', (err) => {
