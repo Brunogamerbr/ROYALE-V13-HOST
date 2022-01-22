8498966a9703b27b052827dcaac2b0cdcbcd82f8
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-22 19:32:45 -0300

    update clear.js

--- Moderação/clear.js
+++ Moderação/clear.js
@@ -1,18 +1,16 @@
 const Discord = require("discord.js");
-exports.run = async (client, message, args, database) => {
-    
+module.exports.run = async (client, message, args, database) => {
     let user = message.author;
     let quantia = args[0];
-if (!quantia){
-  return message.inlineReply(`<:erro:858615784771551252>| Você não colocou o número de mensagens para ser apagadas!`)
-}
+    if (!quantia){
+  return message.reply(`<:erro:858615784771551252>| Você não colocou o número de mensagens para ser apagadas!`)}
 
 if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send(`<:erro:858615784771551252>| Você não possui permissão para usar esse comando!`);
 
   let tanto = 100;
   if (quantia >tanto){
-      return message.inlineReply(`<:erro:858615784771551252>| Eu só consigo apagar de 0 a 100 mensagens..`)
-  }
+      return message.reply(`<:erro:858615784771551252>| Eu só consigo apagar de 0 a 100 mensagens..`)
+}
 
  const msg = await message.channel.send(`<a:load:873563273973338114> Deletando mensagens!`)
   
