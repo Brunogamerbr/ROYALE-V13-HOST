88b5fe974b3135f869e44d44f4798e36a4b37899
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-22 01:02:41 -0300

    update mcbody.js

--- Utilidades/mcbody.js
+++ Utilidades/mcbody.js
@@ -1,12 +1,10 @@
 const Discord = require("discord.js");
-
-
-exports.run = async function(client, message, args, database) {
-  if(!args.length) return message.inlineReply(`:x: **| Por favor, insira o nick de algum jogador de minecraft!**`);
+module exports.run = async function(client, message, args, database) {
+  if(!args.length) return message.reply(`<:erro:858615784771551252>**| Por favor, insira o nick de algum jogador de minecraft!`);
   let embed = new Discord.MessageEmbed()
     .setDescription(`Corpo de ${args[0]}`)
     .setImage(`https://mc-heads.net/body/${args[0]}`)
     .setColor("#0D02FA")
-  message.inlineReply(embed)
+  message.reply(embed)
 }
 
