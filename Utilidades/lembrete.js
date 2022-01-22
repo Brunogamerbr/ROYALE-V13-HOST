3aeebfe1672d4d99e127b9e116452ff573b727d6
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-21 23:39:07 -0300

    update lembrete.js

--- Utilidades/lembrete.js
+++ Utilidades/lembrete.js
@@ -1,39 +1,29 @@
-
 const Discord = require('discord.js');
-
 const ms = require("ms")
-
-
-exports.run = async (client, message, args, database) => {
-
-
-
-  
-        let time = args[0]
-        if(!time) return message.inlineReply("**Para definir um lembrete Use o comando dessa Forma: \`.lembrete [tempo] [mensagem de lembrete]\`**")
-        let lembrete = args.slice(1).join(" ")
-        if(!lembrete) return message.inlineReply(`**Por favor. Defina um texto lembrete**`)
-
-        const seliga = new Discord.MessageEmbed()
-        .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
-     .setTitle (`Lembrete agendado`)
-     .setColor('#0D02FA') 
+module.exports.run = async (client, message, args, database) => {
+  let time = args[0]
+  if(!time) return message.reply("**Para definir um lembrete Use o comando dessa Forma: \`.lembrete [tempo] [mensagem de lembrete]\`**")
+  let lembrete = args.slice(1).join(" ")
+  if(!lembrete) return message.reply(`**Por favor. Defina um texto lembrete**`)
+  const seliga = new Discord.MessageEmbed()
+  .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
+  .setTitle (`Lembrete agendado`)
+  .setColor('#0D02FA') 
   .addField(`Irei lembrar você de:`, `\`${lembrete}\``, true)
   .addField("Daqui a", `\`${time}\``, true) 
-.setTimestamp(Date.now() + ms(time))
- .setFooter(`Lembrete será ocorrido `, client.user.displayAvatarURL())
-        message.inlineReply(seliga)
-
-        setTimeout(() => {
-            let cofoe = new Discord.MessageEmbed()
-            .setTitle("Seu lembrete "  + message.author.username)
-            .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
-            .setDescription(`**${message.author} Seu lembrete ⏰\nLembrete: \`${lembrete}\`
+  .setTimestamp(Date.now() + ms(time))
+  .setFooter(`Lembrete será ocorrido `, client.user.displayAvatarURL())
+  message.reply({embeds: [selig})
+  setTimeout(() => {
+  let cofoe = new Discord.MessageEmbed()
+  .setTitle("Seu lembrete "  + message.author.username)
+  .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
+  .setDescription(`**${message.author} Seu lembrete ⏰\nLembrete: \`${lembrete}\`
 **`)
-                                    .setFooter(`Lembrete Ocorido com sucesso`, client.user.displayAvatarURL())
+.setFooter(`Lembrete Ocorido com sucesso`, client.user.displayAvatarURL())
 .setColor(`#0D02FA`)
-            message.channel.send(message.author, cofoe)
-        }, ms(time))
+ message.channel.send({content: `${message.author}`, embeds: [cofoe]})
+ }, ms(time))
     }
 
  
