36013919554cabbd7eceb52fe9498755824fd49c
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-22 17:46:02 -0300

    update create.js

--- Developers/create.js
+++ Developers/create.js
@@ -1,18 +1,13 @@
 const Discord = require("discord.js")
 const config = require("../config.json");
-exports.run = async (client, message, args, database) => {
+module.exports.run = async (client, message, args, database) => {
   if(!config.owners.includes(message.author.id)) return;
-
   let server = client.guilds.cache.get(args[0]);
-  if(!server) return message.inlineReply("Ei, Servidor inválido!");
-
+  if(!server) return message.reply("Ei, Servidor inválido!");
   let chx = server.channels.cache.filter(chx => chx.type === "text").find(x => x.position === 0);
-
   let invite = await chx.createInvite({
     maxAge: 0,
     maxUses: 0
   });
-
   message.channel.send(`${invite}`);
-        
 }
