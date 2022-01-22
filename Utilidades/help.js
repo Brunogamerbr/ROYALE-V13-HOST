78738e72d7d3a1da3a9919c2df95d706a76237ee
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-22 17:14:36 -0300

    update help.js

--- Utilidades/help.js
+++ Utilidades/help.js
@@ -7,7 +7,7 @@ let embed = new Discord.MessageEmbed()
 \`daily rank work time crime roll rinha rifa coinflip roubar sorte loja empregos depositar sacar pay saldo demissão transacoes\`**\n\n**<a:seta:882102474264154215>| UTILIDADES
 \`donate sugest banner bug invite userinfo afk ping avatar uptime clima\`**\n\n**<a:seta:882102474264154215>| MODERAÇÃO
 \`ban unban kick block setprefix anuncio sorteio setsugest addemoji addvip addcargo say clear\`**\n\n**<a:seta:882102474264154215>| AVENTURA:
-\`create-avatar open battle explorar chop eat buy shop bag av use\`**\n\nGostou de mim? então me adicione em seu servidor clicando **[Aqui](https://discord.com/api/oauth2/authorize?client_id=844227895819894794&permissions=287636319703&scope=bot`)
+\`create-avatar open battle explorar chop eat buy shop bag av use\`**\n\nGostou de mim? então me adicione em seu servidor clicando **[Aqui](https://discord.com/api/oauth2/authorize?client_id=844227895819894794&permissions=287636319703&scope=bot)`)
 .setImage('https://i.imgur.com/Pg9AXWG.gif')
 .setThumbnail(`https://i.imgur.com/y3I6uCc.png`)
 .setFooter(`Mostrando meu Menu de Comandos`)
