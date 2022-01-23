5b13d2fd2560ad11c85fa9991ed90b01b09e0bde
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-01-23 17:25:35 -0300

    update ItensRPG.js

--- Eventos/ItensRPG.js
+++ Eventos/ItensRPG.js
@@ -5,8 +5,12 @@ const db = await client.db.get(`StartRPG_${message.author.id}`)
     if(!db) return;
     const hp = Number(db.hp)
     if(hp <= 0) {
-    await client.db.delete(`StartRPG_${message.author.id}`)
-    await client.db.delete(`InventarioRPG_${message.author.id}`)
-    return message.reply(`${message.author} Você acabou perdendo todos os corações de **HP** você morreu... mas tente iníciar uma nova jornada!`).catch(e => console.log(e.message))
+    setTimeout(function() {
+    client.db.delete(`StartRPG_${message.author.id}`)
+    }, 10);
+    setTimeout(function(){
+    client.db.delete(`InventarioRPG_${message.author.id}`)
+    },10)
+    return message.reply(`${message.author} Você acabou perdendo todos os corações de **HP** você morreu... mas tente iníciar uma nova jornada!`)
     }
 }
