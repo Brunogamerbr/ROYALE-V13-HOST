28669b5457320ae9d74d9d7e38c610b06af9e617
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-02-11 19:45:07 -0300

    update update.js

--- Utilidades/update.js
+++ Utilidades/update.js
@@ -1,22 +1,17 @@
 const Discord = require("discord.js");
 module.exports.run = async(client, message, args, database, prefix) => {
   try{
-  let user = message.author;
-  let dbS = await database.ref(`Sorte/${user.id}`).once("value");
-  let dbSref = database.ref(`Sorte/${user.id}`);
-  let db0 = await database.ref(`Versao/${user.id}`).once("value");
-  let db0ref = database.ref(`Versao/${user.id}`);
-  let db = await database.ref(`VersaoBuild`).once("value");
-  let dbref = database.ref(`VersaoBuild`);
-
-  if (db0.val().versão == db.val().build) return;
+  
+  let db = await client.db.get(`Versao/${message.author.id}`);
+  let dbb = await client.db.get(`VersaoBuild`)
+  
+  if (db.versão == dbb.build) return;
   const ayy = client.emojis.cache.find(emoji => emoji.name === "load");
 
   const msg = await message.reply(`${ayy} Fazendo dowload dos novos recursos..`)
- 
 
-db0ref.set({versão: db.val().build})
 msg.edit(`Você recebeu a atualização: **${db.val().build}**\n<a:verifild:931074474038657024> A maioria dos bugs foram resolvidos!\n\n<a:setaFRS:928016217602654208> Categoria "Diversão" foi removida por não ser muito usada!\n<a:z_fixar:927995349254156338> Novos design em alguns comandos!!`)
+client.db.set(`Versao/${message.author.id}`, {versão: dbb.build})
 }catch(err){
   message.reply(`${err}`)
 }
