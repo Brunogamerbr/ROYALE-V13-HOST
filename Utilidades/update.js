d394b616f338918b5e4f4ee1760238bf0d3a5481
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-02-11 19:34:52 -0300

    update update.js

--- Utilidades/update.js
+++ Utilidades/update.js
@@ -15,11 +15,7 @@ module.exports.run = async(client, message, args, database, prefix) => {
  
 
 setTimeout(() => {
-msg.edit(`${ayy} Adicionando novos itens na sua conta aguarde..`);
-}, 3000)
-
- 
 db0ref.set({versão: db.val().build})
 msg.edit(`Você recebeu a atualização: **${db.val().build}**\n<a:verifild:931074474038657024> A maioria dos bugs foram resolvidos!\n\n<a:setaFRS:928016217602654208> Categoria "Diversão" foi removida por não ser muito usada!\n<a:z_fixar:927995349254156338> Novos design em alguns comandos!!`)
-
+}, 3000)
 }
