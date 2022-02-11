f0e3eff19f24a80c89827e829850f99c326ec06c
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-02-11 19:54:40 -0300

    update update.js

--- Utilidades/update.js
+++ Utilidades/update.js
@@ -10,8 +10,8 @@ module.exports.run = async(client, message, args, database, prefix) => {
 
   const msg = await message.reply(`${ayy} Fazendo dowload dos novos recursos..`)
 
-msg.edit(`Você recebeu a atualização: **${db.build}**\n<a:verifild:931074474038657024> A maioria dos bugs foram resolvidos!\n\n<a:setaFRS:928016217602654208> Categoria "Diversão" foi removida por não ser muito usada!\n<a:z_fixar:927995349254156338> Novos design em alguns comandos!!`)
-client.db.set(`Versao/${message.author.id}`, {versão: dbb.build})
+msg.edit(`Você recebeu a atualização: **${db.versão}**\n<a:verifild:931074474038657024> A maioria dos bugs foram resolvidos!\n\n<a:setaFRS:928016217602654208> Categoria "Diversão" foi removida por não ser muito usada!\n<a:z_fixar:927995349254156338> Novos design em alguns comandos!!`)
+client.db.set(`Versao/${message.author.id}`, {versão: dbb.versão})
 }catch(err){
   message.reply(`${err}`)
 }
