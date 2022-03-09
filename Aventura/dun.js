9a06c36ae0b1561a5eb4b65ea58c8a47362b7a78
Brunogamerbr <91443187+Brunogamerbr@users.noreply.github.com>
2022-03-09 15:32:42 -0300

    update dun.js

--- Aventura/dun.js
+++ Aventura/dun.js
@@ -1,5 +1,11 @@
 const Discord = require("discord.js");
 module.exports.run = async (client, message, args, database, prefix) => {
+ let db = await client.db.get(`Economia/${message.author.id}`);
+ let start = await client.db.get(`StartRPG/${message.author.id}`);
+ 
+ if(start == null) {
+   return message.reply(`<:erro:858615784771551252>| Você ainda não criou seu avatar, para criar um utilize \`${prefix}create-avatar\``)
+ }
 
 
 
